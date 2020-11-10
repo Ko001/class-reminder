20cf0020f1de161ccd38cbbf51f2e682260c15cd
Ko001 <67585038+Ko001@users.noreply.github.com>
2020-11-11 01:28:37 +0900

    undo comment out

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -26,45 +26,45 @@ class WebhookController < ApplicationController
         case event.type
         when Line::Bot::Event::MessageType::Text
           serverURL = "https://class-reminder-bot.herokuapp.com"
-          returnMessage = "a"
+          returnMessage = ""
 
-          # if !User.find_by(line_id: event['source']['userId'])
-          #   if event.message['text'].include?("授業") && event.message['text'].include?("設定")
-          #     returnMessage = "#{serverURL}/users/#{event['source']['userId']}/#{event['replyToken']}/sign_up"
-          #   else
-          #     returnMessage = "「授業設定」と入力して、初期設定をしてください！"
-          #   end
-          # elsif User.find_by(line_id: event['source']['userId'])
-          #   returnMessage = "曜日と時限（半角：1〜7)を指定してね！\n次の授業が知りたいときは、「次」と入力してね！\nあるいは「授業設定」と入力して時間割を編集してね！"
-          #   if event.message['text'].include?("授業") && event.message['text'].include?("設定")
-          #     user = User.find_by(line_id: event['source']['userId'])
-          #     user.pass = event['replyToken']
-          #     user.save
-          #     returnMessage = "#{serverURL}/users/#{event['source']['userId']}/#{event['replyToken']}/sign_in"
-          #     logger.debug("settings")
-          #   elsif event.message['text'].include?("次")
-          #     returnMessage = find_next_class(event['source']['userId']) 
-          #   else
-          #     days = ["日", "月", "火", "水", "木", "金", "土"]
-          #     class_nums = ["1", "2", "3", "4", "5", "6", "7"]
-          #     for i in 0..6 do
-          #       if event.message['text'].include?(days[i])
-          #         for j in 0..6 do
-          #             user = User.find_by(line_id: event['source']['userId'])
-          #             course = user.course_infos.find_by(day: i, time: j + 1)
-          #           if event.message['text'].include?(class_nums[j]) && course.course
-          #             course_time = user.course_times.find_by(class_num: class_nums[j])
-          #             start_time = Time.new(2020, 1, 1, course_time.start_hour, course_time.start_minute, 0)
-          #             end_time = Time.new(2020, 1, 1, course_time.end_hour, course_time.end_minute, 0)
-          #             returnMessage = "#{days[i]}曜#{j+1}限（#{start_time.strftime("%H:%M")}-#{end_time.strftime("%H:%M")}）\n授業名：#{course.course}\n教員名：#{course.prof}\n教室（ZoomID）：#{course.location}\nPass: #{course.pass}"
-          #           elsif event.message['text'].include?(class_nums[j]) && !course.course
-          #             returnMessage = "授業はありません"
-          #           end
-          #         end
-          #       end
-          #     end
-          #   end
-          # end
+          if !User.find_by(line_id: event['source']['userId'])
+            if event.message['text'].include?("授業") && event.message['text'].include?("設定")
+              returnMessage = "#{serverURL}/users/#{event['source']['userId']}/#{event['replyToken']}/sign_up"
+            else
+              returnMessage = "「授業設定」と入力して、初期設定をしてください！"
+            end
+          elsif User.find_by(line_id: event['source']['userId'])
+            returnMessage = "曜日と時限（半角：1〜7)を指定してね！\n次の授業が知りたいときは、「次」と入力してね！\nあるいは「授業設定」と入力して時間割を編集してね！"
+            if event.message['text'].include?("授業") && event.message['text'].include?("設定")
+              user = User.find_by(line_id: event['source']['userId'])
+              user.pass = event['replyToken']
+              user.save
+              returnMessage = "#{serverURL}/users/#{event['source']['userId']}/#{event['replyToken']}/sign_in"
+              logger.debug("settings")
+            elsif event.message['text'].include?("次")
+              returnMessage = find_next_class(event['source']['userId']) 
+            else
+              days = ["日", "月", "火", "水", "木", "金", "土"]
+              class_nums = ["1", "2", "3", "4", "5", "6", "7"]
+              for i in 0..6 do
+                if event.message['text'].include?(days[i])
+                  for j in 0..6 do
+                      user = User.find_by(line_id: event['source']['userId'])
+                      course = user.course_infos.find_by(day: i, time: j + 1)
+                    if event.message['text'].include?(class_nums[j]) && course.course
+                      course_time = user.course_times.find_by(class_num: class_nums[j])
+                      start_time = Time.new(2020, 1, 1, course_time.start_hour, course_time.start_minute, 0)
+                      end_time = Time.new(2020, 1, 1, course_time.end_hour, course_time.end_minute, 0)
+                      returnMessage = "#{days[i]}曜#{j+1}限（#{start_time.strftime("%H:%M")}-#{end_time.strftime("%H:%M")}）\n授業名：#{course.course}\n教員名：#{course.prof}\n教室（ZoomID）：#{course.location}\nPass: #{course.pass}"
+                    elsif event.message['text'].include?(class_nums[j]) && !course.course
+                      returnMessage = "授業はありません"
+                    end
+                  end
+                end
+              end
+            end
+          end
             
           message = {
             type: 'text',
