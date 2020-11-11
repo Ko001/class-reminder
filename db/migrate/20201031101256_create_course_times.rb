d232f920ac433528fb177269ca2f11421854407b
Ko001 <67585038+Ko001@users.noreply.github.com>
2020-11-11 10:36:00 +0900

    match migration files

--- db/migrate/20201031101256_create_course_times.rb
+++ db/migrate/20201031101256_create_course_times.rb
@@ -2,9 +2,10 @@ class CreateCourseTimes < ActiveRecord::Migration[6.0]
   def change
     create_table :course_times do |t|
       t.integer :class_num
-      t.time :start
-      t.time :end
-
+      t.integer :start_hour
+      t.integer :start_minute
+      t.integer :end_hour
+      t.integer :end_minute
       t.timestamps
     end
   end
